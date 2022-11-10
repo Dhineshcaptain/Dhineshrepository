a2eef703fe14d215ae9dada8b8cba42c2d944523
Dhineshcaptain <dhineshravi18@gmail.com>
2022-11-10 15:28:09 +0530

    get git

---
+++ src/test/java/com/trail/Trial.java
@@ -0,0 +1,14 @@
+package com.trail;
+
+public class Trial {
+
+	public void methodA() {
+		System.out.println("a");
+	}
+	public void methodB() {
+		System.out.println("b");
+	}
+	public void methodC() {
+		System.out.println("c");
+	}
+}
